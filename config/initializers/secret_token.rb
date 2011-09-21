596e43c1cd0a3c01452857d208dcef9eb41f72a3
gongz <archerzhang2008@gmail.com>
2011-09-21 16:44:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEPersonalWeb::Application.config.secret_token = '263ade3607c2ab249523c9184133c4b0f98693c900e5d89571aa2a6b6f331f78f5547efdb98279cab16468f18ae7203be03504b98c94163425b7ea6f876bda1a'
---
+++ dance/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dance::Application.config.secret_token = 'fdbb0347e8b7a667596e72432700656cfd06a451ca113f08b8992a6e39f052e819fe8290ca857c19de560a1c661992858b505a60aa5bbc8ba7dbc0503f25073a'
