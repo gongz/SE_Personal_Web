2633aa351a245dc483fdbd952dddad4a52e5274c
gongz <archerzhang2008@gmail.com>
2011-09-23 09:31:36 -0400

    add dance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      sign_in @user
+      redirect_to dance_path
     else
       @title = "Sign up"
       render 'new'
