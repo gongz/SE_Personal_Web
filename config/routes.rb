007689407414c76da3c02759ea9c04fb8bf5017e
gongz <archerzhang2008@gmail.com>
2011-09-23 22:43:46 -0400

    change router root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SEPersonalWeb::Application.routes.draw do
-  get "pages/home"
+
   get "routines/create"
+
   get "routines/destroy"
+
+  root :to => 'pages#home'
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
   resources :routines, :only => [:create, :destroy]
