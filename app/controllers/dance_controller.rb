21bb116f100b9b263c7d95194c1bb8fe4755c382
gongz <archerzhang2008@gmail.com>
2011-09-24 22:07:06 -0400

    vistor_without_add_button

--- app/controllers/dance_controller.rb
+++ app/controllers/dance_controller.rb
@@ -1,9 +1,9 @@
 class DanceController < ApplicationController
   def index
     @dances = Dance.paginate(:page => params[:page],:per_page => 10)
-    @routine = Routine.paginate(:page => params[:page])
     if signed_in?
       @user= current_user
+      @routine = Routine.paginate(:page => params[:page])
     end
   end
 
